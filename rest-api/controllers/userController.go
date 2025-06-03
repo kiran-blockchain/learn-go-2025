124ceac16079cbc88c3f9006b1b1b7beb63cb42a
BRUGUSOFT <kiran.paturi@brugu.io>
2025-06-03 12:00:33 +0530

    added code

--- rest-api/controllers/userController.go
+++ rest-api/controllers/userController.go
@@ -10,57 +10,66 @@ import (
 )
 
 type UserController struct {
-    service services.UserService
+	service services.UserService
 }
 
 func NewUserController(s services.UserService) *UserController {
-    return &UserController{service: s}
+	return &UserController{service: s}
 }
 
 func (c *UserController) GetAllUsers(ctx *gin.Context) {
-    users,err := c.service.GetAllUsers()
-    if err!=nil{
-        ctx.JSON(http.StatusInternalServerError,nil)
-    } else {
-    ctx.JSON(http.StatusOK, users)
-    }
+	users, err := c.service.GetAllUsers()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, nil)
+	} else {
+		token, isExists := ctx.Get("token")
+		if isExists {
+			ctx.JSON(http.StatusOK, gin.H{
+				"jwt":   token,
+				"users": users,
+			})
+		} else {
+            ctx.JSON(http.StatusOK, gin.H{
+				"jwt":   "",
+				"users": users,
+			})
+		}
+	}
 }
 
 func (c *UserController) CreateUser(ctx *gin.Context) {
-   //convert input data to json 
-   var user models.User
-   //convert the payload from postman to model
-    err:= ctx.ShouldBindJSON(&user)
-    user.PasswordHash,err = utils.HashPassword(user.PasswordHash)
-    if(err!=nil){
-        ctx.JSON(http.StatusInternalServerError,nil) 
-    }
-    id,err := c.service.CreateUser(user)
+	//convert input data to json
+	var user models.User
+	//convert the payload from postman to model
+	err := ctx.ShouldBindJSON(&user)
+	user.PasswordHash, err = utils.HashPassword(user.PasswordHash)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, nil)
+	}
+	id, err := c.service.CreateUser(user)
 
-    if err!=nil{
-        ctx.JSON(http.StatusInternalServerError,nil) 
-    }else{
-        ctx.JSON(http.StatusOK,id)
-    }   
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, nil)
+	} else {
+		ctx.JSON(http.StatusOK, id)
+	}
 }
 
-
-
 func (c *UserController) Login(ctx *gin.Context) {
-   //convert input data to json 
-   var user models.User
-   //convert the payload from postman to model
-    err:= ctx.ShouldBindJSON(&user)
-    
-    if(err!=nil){
-        ctx.JSON(http.StatusInternalServerError,nil) 
-    }
-    _,err = c.service.Login(user)
-    if err!=nil{
-        ctx.JSON(http.StatusInternalServerError,nil) 
-    }else{
- 
-        ctx.Next()
-       // ctx.JSON(http.StatusOK,result) 
-    }
+	//convert input data to json
+	var user models.User
+	//convert the payload from postman to model
+	err := ctx.ShouldBindJSON(&user)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, nil)
+	}
+	token, err := c.service.Login(user)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, nil)
+	} else {
+		ctx.Set("token", token)
+		ctx.Next()
+		// ctx.JSON(http.StatusOK,result)
+	}
 }
